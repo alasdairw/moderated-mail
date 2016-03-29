ab6deafebceaecdae59ae1bc08b67d7dc7351630
Alasdair Watson <alasdair@alasdair.biz>
2016-03-29 13:16:30 +0000

    inbound to log

--- app/Http/Controllers/InboundController.php
+++ app/Http/Controllers/InboundController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Log;
 
 class InboundController extends Controller
 {
     //
+    public function recieve(Request $request)
+    {
+        Log::info('request recieved: ',$request->toArray()); 
+
+    }
 }
